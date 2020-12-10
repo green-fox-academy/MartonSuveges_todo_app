57b4a77f3cefc6c92392994fed9abe76ef7361e2
Süveges Márton <marton.suveges@gmail.com>
2020-12-10 14:45:55 +0100

    "Tennivalok kilistazasa" was implemented.

--- ToDoApp/src/com/greenfoxacademy/Main.java
+++ ToDoApp/src/com/greenfoxacademy/Main.java
@@ -1,5 +1,10 @@
 package com.greenfoxacademy;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -12,15 +17,41 @@ public class Main {
     argDescriptions.put("-a", "Új feladatot ad hozzá");
     argDescriptions.put("-r", "Eltávolít egy feladatot");
     argDescriptions.put("-c", "Teljesít egy feladatot");
-
     if (args.length == 0) {
       System.out.println(userInstructions());
+    } else if (checkArg(args[0]) == null) {
+      //TODO: implement unsupported argument handling
+    } else if (args[0].equals("-l")) {
+      printList();
     }
 //    System.out.println("Hello!");
 //    System.out.println(Arrays.toString(args));
 //
 //    Path path = Paths.get("");
 //    System.out.println(path.toAbsolutePath());
+  }
+
+  private static void printList() {
+    Path path = Paths.get("todo.txt");
+    ArrayList<String> lines;
+    try {
+      lines = new ArrayList<>(Files.readAllLines(path));
+    } catch (IOException e) {
+      throw new RuntimeException("Can't find " + path.toAbsolutePath().toString());
+    }
+
+    for (int i = 1; i <= lines.size(); i++) {
+      System.out.println(i + " - " + lines.get(i - 1));
+    }
+  }
+
+  public static String checkArg(String arg) {
+    for (String key : argDescriptions.keySet()) {
+      if (arg.equals(key)) {
+        return key;
+      }
+    }
+    return null;
   }
 
   public static String userInstructions() {
@@ -37,6 +68,4 @@ public class Main {
     }
     return builder.toString();
   }
-
-
 }
